5c2fe6947520c4a59672741f438579b8c3792c12
Jacob Adams <126357606+JacobA12@users.noreply.github.com>
2025-03-15 16:12:12 -0700

    Time: 0 ms (100.00%) | Memory: 56.5 MB (19.56%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -0,0 +1,19 @@
+var trap = function (height) {
+    let left = 0,
+        right = height.length - 1;
+    let ans = 0;
+    let left_max = 0,
+        right_max = 0;
+    while (left < right) {
+        if (height[left] < height[right]) {
+            left_max = Math.max(left_max, height[left]);
+            ans += left_max - height[left];
+            ++left;
+        } else {
+            right_max = Math.max(right_max, height[right]);
+            ans += right_max - height[right];
+            --right;
+        }
+    }
+    return ans;
+};
