e6e1b82ccfb0aff425316b5029d7b2b463a57c98
Jacob Adams <126357606+JacobA12@users.noreply.github.com>
2025-03-19 13:53:32 -0700

    Time: 0 ms (100.00%) | Memory: 53.9 MB (17.90%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    const stack = [root];
+    const output = [];
+
+    while(stack.length != 0){
+        root = stack.pop();
+        if(root){
+            output.push(root.val);
+            if(root.right){
+                stack.push(root.right);
+            }
+            if(root.left){
+                stack.push(root.left);
+            }
+        }
+    }
+
+    return output;
+};
