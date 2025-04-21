3d3f0324e3f4b7ba13a6ff312ae1145ca1778c06
Jacob Adams <126357606+JacobA12@users.noreply.github.com>
2025-04-20 18:29:27 -0700

    Time: 1 ms (86.02%) | Memory: 57 MB (91.65%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -1,18 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
 var levelOrder = function (root) {
-    var levels = [];
-    if (root === null) return levels;
-    var queue = [root];
-    var level = 0;
-    while (queue.length !== 0) {
-        levels.push([]);
-        var level_length = queue.length;
-        for (var i = 0; i < level_length; i++) {
-            var node = queue.shift();
-            levels[level].push(node.val);
-            if (node.left !== null) queue.push(node.left);
-            if (node.right !== null) queue.push(node.right);
-        }
-        level++;
+    if(!root){
+        return [];
     }
-    return levels;
+    
+    let queue = [root];
+    let result = [];
+
+    while (queue.length !== 0) {
+        let levelSize = queue.length;
+        let level = [];
+
+        for (let i = 0; i < levelSize; i++) {
+            let node = queue.shift();
+            level.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        result.push(level);
+    }
+    return result;
 };
